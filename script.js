9f85732773e702a0d7cc44a36a83a50e0ce2ede3
hgarciacyber <165859824+hgarciacyber@users.noreply.github.com>
2024-04-23 23:24:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function generatePassword() {
+    var length = document.getElementById('length').value;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+    var password = "";
+
+    // Limit the length to a maximum of 20
+    length = Math.min(length, 20);
+
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+
+    document.getElementById('password-display').innerText = password;
+}
